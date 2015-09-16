f8c7067a25fc508e57c1f0478189942e83d667b2
Partizanin <topiciv1@yandex.ua>
2015-09-16 13:04:25 +0300

    add file after (VCS error)

---
+++ src/main/java/Conventer/CashValue/InnerExchange.java
@@ -0,0 +1,76 @@
+package Conventer.CashValue;
+
+/**
+ * Created with Intellij IDEA.
+ * Project name: cashTestConverter.
+ * User: Partizanin.
+ * Date: 29.03.2015.
+ * Time:  16:35.
+ * To change this template use File|Setting|Editor|File and Code Templates.
+ */
+public class InnerExchange {
+
+    private String id;
+
+    private String bankName;
+
+    private double buyCourse;
+
+    private double sellCourse;
+
+    public InnerExchange() {
+    }
+
+    public InnerExchange(String id, String bankName, double buyCourse, double sellCourse) {
+        this.id = id;
+        this.bankName = bankName;
+        this.buyCourse = buyCourse;
+        this.sellCourse = sellCourse;
+    }
+
+    public double getCourseByTransactionValue(String transactionValue) {
+        return transactionValue.equals("buy") ? buyCourse : sellCourse;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setBuyCourse(double buyCourse) {
+        this.buyCourse = buyCourse;
+    }
+
+    public void setSellCourse(double sellCourse) {
+        this.sellCourse = sellCourse;
+    }
+
+    public double getBuyCourse() {
+        return buyCourse;
+    }
+
+    public double getSellCourse() {
+        return sellCourse;
+    }
+
+    @Override
+    public String toString() {
+        return "InnerExchange{" +
+                "id='" + id + '\'' +
+                ", bankName='" + bankName + '\'' +
+                ", buyCourse=" + buyCourse +
+                ", sellCourse=" + sellCourse +
+                '}';
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+}
---
+++ src/main/java/servlets/StartServlet.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Partizanin on 13.09.2015.
+ */
+@WebServlet(name = "StartServlet",urlPatterns = "/StartServlet")
+public class StartServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
